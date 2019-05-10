d9f578d04eeeab64de33fb0ddb1a7c28f62a7784
USER <USER@USER-PC>
2019-05-10 18:22:11 +0900

    new/dev : 작업중....!

--- 08.Model2MVCShop(RestFul Server)/src/main/java/com/model2/mvc/web/user/UserRestController.java
+++ 08.Model2MVCShop(RestFul Server)/src/main/java/com/model2/mvc/web/user/UserRestController.java
@@ -1,26 +1,26 @@
 package com.model2.mvc.web.user;
 
+import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.model2.mvc.common.Page;
 import com.model2.mvc.common.Search;
 import com.model2.mvc.service.domain.User;
 import com.model2.mvc.service.user.UserService;
+
+import jdk.nashorn.api.scripting.JSObject;
 
 
 //==> 회원관리 RestController
@@ -102,6 +102,42 @@ public class UserRestController {
 		}
 		
 		return (User)session.getAttribute("user");
+	}
+	
+	@RequestMapping(value="json/checkDuplication",method=RequestMethod.POST)
+	public Map<String,Object> checkDuplication(@RequestBody String userId) throws Exception{
+		System.out.println("/user/json/checkDuplication : POST");
+		
+		boolean result=userService.checkDuplication(userId);
+		
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("result", new Boolean(result));
+		map.put("userId", userId);
+
+		return map;
+	}
+	
+	@RequestMapping(value="json/listUser")
+	public Map<String,Object> listUser(@RequestBody Search search) throws Exception{
+		System.out.println("/user/json/listUser : GET / POST");
+		
+		if(search.getCurrentPage() ==0 ){
+			search.setCurrentPage(1);
+		}
+		search.setPageSize(pageSize);
+		
+		// Business logic 수행
+		Map<String , Object> map=userService.getUserList(search);
+		
+		Page resultPage = new Page( search.getCurrentPage(), ((Integer)map.get("totalCount")).intValue(), pageUnit, pageSize);
+		System.out.println(resultPage);
+		
+		Map<String,Object> hashMap = new HashMap<String,Object>();
+		hashMap.put("list", map.get("list"));
+		hashMap.put("resultPage", resultPage);
+		hashMap.put("search", search);
+		
+		return hashMap;
 	}
 	
 }
